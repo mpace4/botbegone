867ac6256314b832f2fe3517df57dd7d75555c3c
ythuang13 <60416834+ythuang13@users.noreply.github.com>
2020-05-14 10:54:39 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,11 +1,15 @@
 import model
+import view
 
 def main():
-    finalproj.main()
-    # verify username
-    # send over to model module to process
-    # send the processed data from model to view to present it
-    # ask if user want to do another one
+    username = view.twitter_username_input()
+    try:
+        model.validate_username(username)
+    except ValueError:
+        print("Invalid Username")
+        main()
+    outputList = model.bot_points(username)
+    view.display_output(outputList)
     
 if __name__ == "__main__":
     main()
