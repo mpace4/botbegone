090139f52573f498464a01859057a89a11e09be0
ythuang13 <60416834+ythuang13@users.noreply.github.com>
2020-05-14 10:54:20 -0700

    Update model.py

--- model.py
+++ model.py
@@ -14,28 +14,27 @@ api = tweepy.API(auth)
 
 tweetTime = []
 tweetText = []
+outputList = []
 rtcount = 0
 count = 20
-username = ""
 botPoints = 0
-checkDatePoints = 0
-picPoints = 0
-tweetCheckerPoints = 0
-RTPoints = 0
-tweetDatePoints = 0
-bioPoints = 0
 
-def search():
-    global username, acctDesc, tweetText, location, following, follwers
-    global totaltweets, usercreatedts, count, profile_pic
-    
-    username = view.twitter_username_input()
-    
-    try: # check for invalid username
+
+# validate username
+def validate_username(username:str) -> None:
+    """validate if username is valid or not, if not, raise Value Error"""
+    try:
         dummy = api.user_timeline(id=username, count=1)
+        del dummy
     except tweepy.TweepError:
-        print("Incorrect username")
-        username = view.twitter_username_input()
+        raise ValueError
+        
+
+def search(username:str):
+    """search and process user information: username, account description,
+        tweets, location, folllwing, followers, profile picture"""
+    global acctDesc, tweetText, location, following, follwers
+    global totaltweets, usercreatedts, count, profile_pic
     
     for tweet in api.user_timeline(id=username, count=count):
         totaltweets = tweet.user.statuses_count
@@ -52,8 +51,9 @@ def search():
     usercreatedts = tweet.user.created_at
     print(acctDesc)
     profile_pic = tweet.user.profile_image_url
-        
-def rtcounter()-> None:
+
+
+def rtcounter() -> None:
     global rtcount
     num = 0
     counter = count
@@ -65,13 +65,14 @@ def rtcounter()-> None:
                 counter = counter - 1
                 num = num - 2
         num = num + 1
-        
-def tweetChecker()->None:
-    global botPoints, tweetCheckerPoints
-    num = 0
+
+
+def tweetChecker() -> None:
+    global botPoints
+    num = length = 0
     counter = count - rtcount
-    length = 0
-    num_compare = 0
+    num_compare = 1
+    tweetCheckerPoints = 0
     while (num < counter - 1):
         first_list = tweetText[num]
         first_list = [word.strip(",.()/*-+`~!@#$%^&*;=<>?")
@@ -89,21 +90,19 @@ def tweetChecker()->None:
             length = len(set1) + len(set2)
             total = (len(intersection) * 2) / length 
             if total >= .50:
-                botPoints = botPoints + 5
                 tweetCheckerPoints = 5
             elif total >= .35:
-                botPoints = botPoints + 3
                 tweetCheckerPoints = 3
             elif total >= .20:
-                botPoints = botPoints + 1
                 tweetCheckerPoints = 1
             element = element + 1
             num_compare = num_compare + 1
-    botPoints = botPoints / num_compare
     tweetCheckerPoints = tweetCheckerPoints / num_compare
-    if botPoints < 1:
-        botPoints = 0
-   
+    if tweetCheckerPoints < 1:
+        tweetCheckerPoints = 0
+    outputList.append(f"tweet checker: {tweetCheckerPoints} out of 5")
+    botPoints += tweetCheckerPoints
+
 
 def checkRT():
     global botPoints, RTPoints
@@ -119,9 +118,9 @@ def checkRT():
         RTPoints = 1
 
 
-def tweetDateTime()-> None:
+def tweetDateTime() -> None:
     global tweetTime
-    global botPoints, tweetDatePoints
+    global botPoints
     tweetElem = count - 1
     pairs = tweetElem
     num = 0
@@ -134,6 +133,7 @@ def tweetDateTime()-> None:
     minstart = 14
     minend = 16
     matches = 0
+    tweetDatePoints = 0
     while (num < tweetElem):
         list1 = str(tweetTime[num])
         list2 = str(tweetTime[num + 1])
@@ -155,48 +155,56 @@ def tweetDateTime()-> None:
                 matches = matches + 1
     total = matches / pairs
     if total >= .50:
-        botPoints = botPoints + 5
         tweetDatePoints = 5
     elif total >= .35:
-        botPoints = botPoints + 3
         tweetDatePoints = 3
     elif total >= .20:
-        botPoints = botPoints + 1
         tweetDatePoints = 1
+    outputList.append(f"tweet time: {tweetDatePoints} out of 5")
+    botPoints += tweetDatePoints
 
 
-def checkPic():
-    global profile_pic, botPoints, picPoints
-    
-    if profile_pic == "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png":
-        botPoints = botPoints + 5
+def checkPic() -> None:
+    """check if user have default profile picture"""
+    global profile_pic, botPoints
+    picPoints = 0
+    if profile_pic == "http://abs.twimg.com/sticky/default_profile_images/"\
+                        "default_profile_normal.png":
         picPoints = 5
-        print(picPoints)
+    
+    outputList.append(f"profile picture: {picPoints} out of 5")
+    botPoints += picPoints
 
-def checkDate():
-    global usercreatedts, botPoints, checkDatePoints
+def checkDate() -> None:
+    """Check how new the user is"""
+    global usercreatedts, botPoints
     current_date = datetime.now()
-        
+    checkDatePoints = 0
     if usercreatedts > (datetime.now() - timedelta(days = 7)):
-        botPoints = botPoints + 5
         checkDatePoints = 5
     elif usercreatedts > (datetime.now() - timedelta(days = 30)):
-        botPoints = botPoints + 3
         checkDatePoints = 3
     elif usercreatedts > (datetime.now() - timedelta(days = 60)):
-        botPoints = botPoints + 1
         checkDatePoints = 1
-        
-def checkBio():
-    global botPoints, acctDesc, picPoints, bioPoints
+
+    outputList.append(f"check date: {checkDatePoints} out of 5")
+    botPoints += checkDatePoints
+
+
+def checkBio() -> None:
+    """Check user bio if it's empty"""
+    global botPoints, acctDesc
+    bioPoints = 0
     if acctDesc == "":
-        botPoints = botPoints + 3
         bioPoints = 3
-        
-    
-def main():
-    mainList = []
-    search()
+    outputList.append(f"check bio: {bioPoints} out of 3")
+    botPoints += bioPoints
+
+
+def bot_points(username:str) -> list:
+    """take a string of username, process the bot points,
+        return an output of list"""
+    search(username)
     rtcounter()
     tweetChecker()
     checkPic()
@@ -204,11 +212,6 @@ def main():
     checkRT()
     checkBio()
     tweetDateTime()
-    print(picPoints)
-    mainList = [f"tweet time: {tweetDatePoints} out of 5",f"check date: {checkDatePoints} out of 5", 
-                f"profile picture: {picPoints} out of 5", f"tweet checker: {tweetCheckerPoints} out of 5",
-                f"check bio: {bioPoints} out of 3", f"total bot points: {botPoints}"]
-    view.display_output(mainList)
-    #print("bot points: " + str(botPoints))
-
+    outputList.append(f"total bot points: {botPoints}")
     
+    return outputList
